3a3fb70517bc84bca15ecc0f74232729a339704f
Dericus Horner He/Him <123376826+Dhorner4@users.noreply.github.com>
2023-03-22 19:16:39 -0400

    Create ops301-challenge08.py

---
+++ ops301-challenge08.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+#Script: Ops 301 Class 08 Ops Challenge Solution
+# Author: Dericus Horner
+# Date of latest revision:03/22/2023
+# Assistance from Chat GPT
+
+# Main 
+
+my_list = ["cherry", "canalope", "plum", "grape", "kiwi", "mango", "peach", "pear", "pineapple", "watermelon"]
+
+# print the fourth element of the list
+print(my_list[3])
+
+# print the sixth through tenth elements of the list
+print(my_list[5:])
+ 
+# change the value of the seventh element to "onion"
+my_list[6] = "onion"
+print(my_list)
+
+# Done
