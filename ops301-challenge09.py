2ded7255ac51766ff25d530aeb61024c3cc8260f
dericush <hornerdericus@gmail.com>
2023-03-23 21:04:30 +0000

    Challenge 09 Complete

---
+++ ops301-challenge09.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+#Script: Ops 301 Class 08 Ops Challenge Solution
+# Author: Dericus Horner
+# Date of latest revision:03/22/2023
+# Assistance from Chat GPT
+
+# Main
+
+a = 10
+b = 5
+
+# Equals
+if a == b:
+    print("a is equal to b")
+
+# Not Equals
+if a != b:
+    print("a is not equal to b")
+
+# Less than
+if a < b:
+    print("a is less than b")
+
+# Less than or equal to
+if a <= b:
+    print("a is less than or equal to b")
+
+# Greater than
+if a > b:
+    print("a is greater than b")
+
+# Greater than or equal to
+if a >= b:
+    print("a is greater than or equal to b")
+
+    # Done
